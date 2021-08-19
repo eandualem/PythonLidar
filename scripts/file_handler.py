c4c436f4a41f226fb4e37056cbdf22031fc22701
Elias <eandualem@gmail.com>
2021-08-19 11:23:32 +0300

    feat: clean elivation extractor class

--- scripts/file_handler.py
+++ scripts/file_handler.py
@@ -1,4 +1,5 @@
 import json
+import laspy
 import pandas as pd
 from config import Config
 from log import get_logger
@@ -7,41 +8,52 @@ from log import get_logger
 class FileHandler():
 
   def __init__(self):
-    self.logger = get_logger("FileHandler")
+    self._logger = get_logger("FileHandler")
+
+  def save_csv(self, df, name, index=False):
+    try:
+      path = Config.ASSETS_PATH / str(name + '.csv')
+      df.to_csv(path, index=index)
+      self._logger.info(f"{name} is saved successfully in csv format")
+    except Exception:
+      self._logger.exception(f"{name} save failed")
+
+  def read_csv(self, name, missing_values=[]):
+    try:
+      path = Config.ASSETS_PATH / str(name + '.csv')
+      df = pd.read_csv(path, na_values=missing_values)
+      self._logger.info(f"{name} read successfully")
+      return df
+    except FileNotFoundError:
+      self._logger.exception(f"{name} not found")
 
   def read_json(self, name):
     try:
       path = Config.ASSETS_PATH / str(name + '.json')
       with open(path, 'r') as json_file:
         json_obj = json.load(json_file)
-      self.logger.info(f"{name} read successfully")
+      self._logger.info(f"{name} read successfully")
       return json_obj
     except Exception:
-      self.logger.exception(f"{name} not found")
+      self._logger.exception(f"{name} not found")
 
   def read_txt(self, name):
     try:
       path = Config.ASSETS_PATH / str(name + '.txt')
       with open(path, "r") as f:
         text_file = f.read().splitlines()
-      self.logger.info(f"{name} read successfully")
+      self._logger.info(f"{name} read successfully")
       return text_file
     except Exception:
-      self.logger.exception(f"{name} not found")
+      self._logger.exception(f"{name} not found")
 
-  def save_csv(self, df, name, index=False):
+  def read_point_data(self, name) -> dict:
     try:
-      path = Config.ASSETS_PATH / str(name + '.csv')
-      df.to_csv(path, index=index)
-      self.logger.info(f"{name} is saved successfully in csv format")
-    except Exception:
-      self.logger.exception(f"{name} save failed")
-
-  def read_csv(self, name, missing_values=[]):
-    try:
-      path = Config.ASSETS_PATH / str(name + '.csv')
-      df = pd.read_csv(path, na_values=missing_values)
+      path = Config.ASSETS_PATH / str(name + '.txt')
+      las = laspy.read(path)
       self.logger.info(f"{name} read successfully")
-      return df
-    except FileNotFoundError:
+      return las
+    except Exception:
       self.logger.exception(f"{name} not found")
+
+
