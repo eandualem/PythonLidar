a25464a8b854f82a473c98f9541c986b7b2c9be9
Elias <eandualem@gmail.com>
2021-08-19 08:09:54 +0300

    feat: update web scrapping script to show progress

--- scripts/web_scrapping.py
+++ scripts/web_scrapping.py
@@ -11,11 +11,15 @@ print(len(datasets))
 
 df = pd.DataFrame(columns=['dataset', 'bounds', 'boundsConforming', 'points'])
 http = urllib3.PoolManager()
+i = 0
 for d in datasets:
   r = http.request('GET', target_url + d + "ept.json")
   if r.status == 200:
     j = json.loads(r.data)
     df = df.append({'dataset': d, 'bounds': j['bounds'], 'boundsConforming': j['boundsConforming'], 'points': j['points']}, ignore_index=True)
+    if(i%100 == 0):
+      print(f"{(i / len(datasets)) * 100}%")
+    i+=1
 
 # print(df.head())
 df.to_csv(r'../data/metadata.csv', index=False, header=True)
