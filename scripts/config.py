d43c2738d25ef829b44c775e67bb222c809b4d83
Elias <eandualem@gmail.com>
2021-08-20 07:54:58 +0300

    feat: get data from multiple regions

--- scripts/config.py
+++ scripts/config.py
@@ -3,10 +3,11 @@ from pathlib import Path
 
 class Config:
   RANDOM_SEED = 27
-  ROOT_PATH = Path("../")
+  ROOT_PATH = Path("./")
   REPO = "https://github.com/eandualem/PythonLidar"
+  LOG_FILE = ROOT_PATH / "log/PythonLidara.log"
   DATA_PATH = ROOT_PATH / "data/"
-  ASSETS_PATH = ROOT_PATH / "asset/"
+  ASSETS_PATH = ROOT_PATH / "assets/"
   LAZ_PATH = DATA_PATH / "laz"
   TIF_PATH = DATA_PATH / "shp"
   SHP_PATH = DATA_PATH / "tif"
--- scripts/get_data.py
+++ scripts/get_data.py
@@ -4,9 +4,9 @@ from config import Config
 from df_generator import DfGenerator
 from log import get_logger
 from file_handler import FileHandler
+from bounds import Bounds
 
 PUBLIC_DATA_PATH = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/"
-PIPELINE_PATH = "./get_data.json"
 
 
 class GetData:
@@ -15,61 +15,65 @@ class GetData:
                metadata_filename: str = "",
                ):
     self._public_access_path = public_access_path
-    self._metadata_filename = metadata_filename
     self._df_generator = DfGenerator()
     self._logger = get_logger("FileHandler")
     self._file_handler = FileHandler()
+    self._metadata = self._file_handler.read_csv(metadata_filename)
 
   def get_pipeline(self, bounds, regions, filename):
-    pipe = self._file_handler.read_json(PIPELINE_PATH)
+    pipe = self._file_handler.read_json("usgs_3dep_pipeline")
 
     pipe['pipeline'][0]['bounds'] = bounds
     pipe['pipeline'][0]['filename'] = self._public_access_path + regions + "/ept.json"
-    pipe['pipeline'][3]['filename'] = "../data/laz/" + filename + ".laz"
-    pipe['pipeline'][4]['filename'] = "../data/tif/" + filename + ".tif"
+    pipe['pipeline'][3]['filename'] = str(Config.LAZ_PATH / str(filename + ".laz"))
+    pipe['pipeline'][4]['filename'] = str(Config.LAZ_PATH / str(filename + ".tif"))
     return pdal.Pipeline(json.dumps(pipe))
 
   def check_cache(self):
-    # TODO:
+    # TODO: 
     pass
 
-  def get_regions(self, bounds: str):
-    df = self._file_handler.read_csv(self._metadata_filename)
-    filtered_df = df[
-      df['xmin'] > bounds[0][0]
-      and df['xmax'] > bounds[0][1]
-      and df['ymin'] > bounds[1][0]
-      and df['ymax'] > bounds[1][1]
+  def get_bound_metadata(self, bounds: Bounds):
+
+    filtered_df = self._metadata.loc[
+      (self._metadata['xmin'] <= bounds.xmin)
+      & (self._metadata['xmax'] >= bounds.xmax)
+      & (self._metadata['ymin'] <= bounds.ymin)
+      & (self._metadata['ymax'] >= bounds.ymax)
     ]
-    return filtered_df[["dataset"]]
+    return filtered_df[["filename", "region", "year"]]
 
   def get_geo_data(self, bounds, region, filename) -> None:
     # TODO: check in chache first
     pl = self.get_pipeline(bounds, region, filename)
     try:
       pl.execute()
-      # metadata = pl.metadata
       geo_data = self._df_generator.get_geo_data(filename)
-      self._logger(f"successfully read geo data: {filename}")
+      self._logger.info(f"successfully read geo data: {filename}")
       return geo_data
     except RuntimeError as e:
-      self._logger(f"error reading geo data, error: {e}")
+      self._logger.exception(f"error reading geo data, error: {e}")
 
   def get_data(self, bounds: str) -> None:
     list_geo_data = []
-    for region in self.get_regions():
-      filename = region + "_".join(bounds)
-      list_geo_data.append(self.get_geo_data(filename))
+    df_meta = self.get_bound_metadata(bounds)
+    for index, row in df_meta.iterrows():
+      filename = row['filename'].replace('/', '')
+      data = self.get_geo_data(bounds.get_bound_str(), filename, filename)
+      list_geo_data.append(data)
+
     return list_geo_data
 
 
 # Test
-get_data = GetData()
-get_data.get_raster_terrain(
-    bounds="([-10425171.940, -10423171.940], [5164494.710, 5166494.710])",
-    regions="IA_FullState",
-    output_filename="iowa",
-)
+bounds = Bounds(-10436887.43333523, -10435905.484106943, 5148706.389047224, 5149217.145836504)
+# bounds = Bounds(-10425171.940, -10423171.940, 5164494.710, 5166494.710)
+get_data = GetData(metadata_filename="usgs_3dep_metadata")
+get_data.get_data(bounds)
+
+# get_data.get_raster_terrain(
+#     bounds="([-10425171.940, -10423171.940], [5164494.710, 5166494.710])",
+# )
 
 # get_data.get_raster_terrain(
 #     bounds="([-11669524.7, -11666600.81], [4776607.3, 4778714.4])",
